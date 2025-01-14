e29b23476028d452b036296e245cb01e70741152
RealMinTech <Adesinamariam7@gmail.com>
2025-01-14 06:07:38 -0600

    crud done

--- controller/book-controller.js
+++ controller/book-controller.js
@@ -50,4 +50,89 @@ const getSingleBookById = async (req, res) => {
   }
 };
 
-module.exports = { getAllBooks };
+const addNewBook = async (req, res) => {
+  try {
+    const newBookFormData = req.body
+    const newlyCreatedBook = await Book.create(newBookFormData)
+    if(newBookFormData) {
+      req.status(201).json({
+        success: true,
+        message: "Book added successfully",
+        data: newlyCreatedBook,
+      })
+    }
+  } catch (error) {
+    console.log(e);
+    res.status(500).json({
+      success: false,
+      message: "Something went wrong! Please try again"
+    })
+  }
+}
+
+const updateBook = async (req, res) => {
+  try {
+    const updateBookData = req.body
+    const getCurrentBookID = req.params.id
+    const updatedBook = await Book.findByIdAndUpdate(
+      getCurrentBookID,
+      updateBookData,
+      {
+        new: true,
+      }
+    )
+
+    if(!updateBook) {
+      res.status(404).json({
+        success: false,
+        message: "Book is not found with the specified ID"
+      })
+    }
+
+    res.status(200).json({
+      success: true,
+      message: "Book updated successfully"
+    })
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
+      success: false,
+      message: "Something went wrong! Please try again"
+    })
+    
+  }
+}
+
+const deleteBook = async (req, res) => {
+  try {
+    const getCurrentBookId = req.params.id
+    const getDeletedBook = await Book.findByIdAndDelete(getCurrentBookId)
+
+    if(!deleteBook) {
+      res.status(404).json({
+        success: false,
+        message: "Book is not found with this ID"
+      })
+    }
+
+    res.status(200).json({
+      success: true,
+      data: getDeletedBook,
+    })
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
+      success: false,
+      message: "Something went wrong! Please try again"
+    })
+    
+  }
+}
+
+module.exports = {
+  getAllBooks,
+  getSingleBookById,
+  addNewBook,
+  updateBook,
+  deleteBook,
+};
--- routes/book-routes.js
+++ routes/book-routes.js
@@ -1,3 +1,20 @@
-const express = require("express")
+const express = require("express");
 
-const {} = require("../controller/book-controller")
+const {
+  getAllBooks,
+  getSingleBookById,
+  addNewBook,
+  updateBook,
+  deleteBook
+} = require("../controller/book-controller");
+
+
+const router = express.Router()
+
+router.get("/get", getAllBooks)
+router.get("/get/:id", getSingleBookById);
+router.post("/add", addNewBook);
+router.put("/update/:id", updateBook);
+router.delete("/delete/:id", deleteBook);
+
+module.exports = router;
